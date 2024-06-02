bcc4f522b7db397becd37cd7f668b9dd4fd94b3a
omkark-16 <155129274+omkark-16@users.noreply.github.com>
2024-06-02 14:40:08 +0530

    Add files via upload

---
+++ words.js
@@ -0,0 +1,42 @@
+const wordList = [
+    {
+        word: "india",
+        hint: "our country name"
+    },
+    {
+        word: "silk",
+        hint: "your favourite choclate name"
+    },
+    {
+        word: "omkar",
+        hint: "name of your mama"
+    },
+    {
+        word: "dog",
+        hint: "which animal is your favourite"
+    },
+    {
+        word: "gold",
+        hint: "a yellow precious metal"
+    },
+    {
+        word: "meeshow",
+        hint: "on which site you are done shopping"
+    },
+    {
+        word: "nanded",
+        hint: "what is your district name"
+    },
+    {
+        word: "pencil",
+        hint: "which thing you need to write on the noteboook"
+    },
+    {
+        word: "twelve",
+        hint: "5+5+2"
+    },
+    {
+        word: "31",
+        hint: "how much days in the january month"
+    },
+]
